b3aa02e59461ea660588427fb3e11abf6e174df3
Logickin-Lambda <LogickinLambda@protonmail.com>
2025-09-08 19:21:01 +0800

    - Remove import for C file and headers With the new design, we no longer need to use the original sunvox.h because the share library is now load directly from zig.
    I still keep the header though since I still need
    the function for reference.

--- build.zig
+++ build.zig
@@ -1,47 +1,18 @@
 const std = @import("std");
 
-// Although this function looks imperative, it does not perform the build
-// directly and instead it mutates the build graph (`b`) that will be then
-// executed by an external runner. The functions in `std.Build` implement a DSL
-// for defining build steps and express dependencies between them, allowing the
-// build runner to parallelize the build automatically (and the cache system to
-// know when a step doesn't need to be re-run).
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zunvox", .{
+    const zunvox = b.addModule("zunvox", .{
         .root_source_file = b.path("src/zunvox.zig"),
         .target = target,
-    });
-
-    const sunvox = b.addModule("sunvox", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const sunvox_lib = b.addLibrary(.{
-        .name = "sunvox",
-        .root_module = sunvox,
-        .linkage = .static,
-    });
-
-    b.installArtifact(sunvox_lib);
-
-    sunvox.addIncludePath(b.path("libs/sunvox"));
-    sunvox_lib.linkLibCpp();
-    sunvox_lib.linkLibC();
-
-    sunvox.addCSourceFile(.{
-        .file = b.path("libs/sunvox/sunvox.c"),
-        .flags = &.{
-            "-DSUNVOX_MAIN",
-        },
     });
 
     // let's only consider 64bit windows first before moving onto other system
     b.getInstallStep().dependOn(&b.addInstallBinFile(b.path("libs/sunvox/windows/lib_x86_64/sunvox.dll"), "sunvox.dll").step);
 
+    // setting up test
     const test_step = b.step("test", "zunvox tests");
 
     const test_module = b.addModule("test", .{
@@ -55,10 +26,7 @@ pub fn build(b: *std.Build) void {
         .root_module = test_module,
     });
 
-    tests.linkLibrary(sunvox_lib);
-    tests.root_module.addLibraryPath(b.path("libs/sunvox/windows/lib_x86_64"));
-    tests.root_module.linkSystemLibrary("sunvox", .{});
-
+    tests.root_module.addImport("zunvox", zunvox);
     b.installArtifact(tests);
 
     var test_step_run = b.addRunArtifact(tests);
